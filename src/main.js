c501c85f39ccc0bac689bbddd58eae52ef44b221
Olegus <oleggus@gmail.com>
2026-02-03 23:57:31 -0500

    add PWA manifest

--- src/main.js
+++ src/main.js
@@ -887,4 +887,18 @@ function drawDot(r, c, color, pathId) {
   ctx.fillText(pathId.toString(), x, y + (DOT_RADIUS * 0.1));
 }
 
+
+// Register Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./sw.js')
+      .then(registration => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      })
+      .catch(err => {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+  });
+}
+
 init();
---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'connect-dots-v1';
+const ASSETS_TO_CACHE = [
+    './index.html',
+    './src/style.css',
+    './src/main.js',
+    './src/Grid.js',
+    './src/Generator.js',
+    './src/Random.js',
+    './icon.png',
+    './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Opened cache');
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            // Cache hit - return response
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
